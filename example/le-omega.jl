eeca47fa341d7acb6c5dc82537db21afeb6e525d
Felix Gerick <felixgerick@gmail.com>
2019-06-19 15:13:52 +0200

    add other rms ratio function

--- example/le-omega.jl
+++ example/le-omega.jl
@@ -12,6 +12,8 @@ const a = 1.25
 const b = 0.8
 const c = 1.0
 
+cmat = Mire.cacheint(N,a,b,c)
+
 @everywhere begin
     MIREPATH=joinpath(dirname(pathof(Mire)),"..")
     include(joinpath(MIREPATH,"src/misc/analysis.jl"))
@@ -21,7 +23,18 @@ const c = 1.0
     function rmsp(us,N,vs,a,b,c,n,ngrid)
         [rmsle(N, vs, us[1:end÷2,i]/mean(us[:,i]), a, b, c,ngrid) for i=1:n]
     end
+
+    function rms_parperp(N,vs,cmat,u)
+    v_ile=Mire.eigenvel(N,vs,real.(u),a,b,c,norm=false)
+    v_perp = v_ile[1]^2+v_ile[2]^2
+    v_par = v_ile[3]^2
+    rms_par=Mire.int_polynomial_ellipsoid(v_perp,cmat)
+    rms_perp=Mire.int_polynomial_ellipsoid(v_par,cmat)
+    return real(rms_perp)/real(rms_par)
 end
 
-rms_all = pmap(u->rmsp(u,N,vs,a,b,c,n,30),us)
-BSON.@save joinpath(datapath,"rms_all_mire.bson") rms_all
+end
+
+# rms_all = pmap(u->rmsp(u,N,vs,a,b,c,n,30),us)
+rms_all = pmap(u->rms_parperp(N,vs,cmat,u),us)
+BSON.@save joinpath(datapath,"rms_all_mire_parperp.bson") rms_all
