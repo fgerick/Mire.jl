3225c00dd339b4f24377cc8b62c9f16ff6b4a07e
Felix Gerick <felixgerick@gmail.com>
2019-07-15 13:30:44 +0200

    add A formulation b0 to convergence test

--- example/convergence.jl
+++ example/convergence.jl
@@ -11,12 +11,15 @@ Le = 1e-6
 Ω = 1/Le *Mire.ez
 α = 0.01
 v0_wu = [0,-x*z/c^2,x*y/b^2]
-b0 = [-Mire.y/b^2,Mire.x/a^2,0] .+ α*v0_wu
+v0_A = [c^2*x*y*(2 - (2x^2)/a^2 - (5y^2)/b^2),
+        c^2*y^2*(-1 + (4x^2)/a^2 + y^2/b^2),
+        c^2*y*(-((2x^2)/a^2) + y^2/b^2)*z]
+b0 = [-Mire.y/b^2,Mire.x/a^2,0] .+ α*v0_A
 
 N=3:2:35
 
 for N in N
     cmat = Mire.cacheint(N,a,b,c)
     @time LHS,RHS, vs = assemblemhd(N,cmat,a,b,c,Ω,b0)
-    BSON.@save joinpath(datapath,"mire_high_order_matrices_N$(N).bson") LHS RHS vs a b c Le
+    BSON.@save joinpath(datapath,"mire_high_order_matrices_A_N$(N).bson") LHS RHS vs a b c Le
 end
