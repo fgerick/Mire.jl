2ab97cf030905bee72f38cc6b07b254c3c975ce5
Felix Gerick <felixgerick@gmail.com>
2019-09-22 14:51:19 +0200

    up travis and deploydocs

--- docs/make.jl
+++ docs/make.jl
@@ -1,6 +1,6 @@
 using Documenter, Mire
 
 makedocs(sitename = "Mire.jl - Modes In Rotating Ellipsoids Julia package")
-# deploydocs(
-#     repo = "github.com/fgerick/Mire.jl.git",
-# )
+deploydocs(
+    repo = "github.com/fgerick/Mire.jl.git",
+)
