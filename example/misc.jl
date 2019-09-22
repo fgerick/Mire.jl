2cec3d0f27470a84e6e4c531e6f230b6b5b880ac
Felix Gerick <felixgerick@gmail.com>
2019-09-22 17:20:26 +0200

    up notebook

--- example/misc.jl
+++
@@ -1,8 +0,0 @@
-
-"""
-    angularmom(u,a,b,c)
-
-Calculates z-component of angular momentum.
-"""
-angularmom(u,a,b,c) = int_polynomial_ellipsoid((u×r)[3],a,b,c)
-angularmom(u,cmat) = int_polynomial_ellipsoid((u×r)[3],cmat)
