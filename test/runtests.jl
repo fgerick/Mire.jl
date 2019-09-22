58ea754bb632860b42bbb3cfac1460bfde10c0d1
Felix Gerick <felixgerick@gmail.com>
2019-09-22 16:46:51 +0200

    add qg inertial mode test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,16 @@
-using Test, Mire
+using Test, LinearAlgebra, Mire
 
-@test true
+
+@testset "QG Inertial modes" begin
+    a,b,c=1.,1.,1.
+    Ω = [0,0,1]
+    N = 3
+    B,A, uj = assemblehd(N, a, b, c, Ω)
+    esol = eigen(inv(Matrix(B))*A)
+    ω = esol.values
+
+    # Zhang et al., J. Fluid Mech. (2001), vol. 437, pp. 103–119. eq (4.5)
+    zhang(m,N)=-2/(m+2)*(√(1+m*(m+2)/(N*(2N+2m+1)))-1)*im
+    @test any(esol.values.≈zhang(1,1))
+    @test any(esol.values.≈zhang(2,1))
+end
