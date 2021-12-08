2aa4f0344e2e174f696365ccc3507d5bbad197e7
Felix <felixgerick@gmail.com>
2021-12-08 11:39:42 +0100

    add doc configuration

--- docs/make.jl
+++ docs/make.jl
@@ -3,4 +3,5 @@ using Documenter, Mire
 makedocs(sitename = "Mire.jl - Modes In Rotating Ellipsoids Julia package")
 deploydocs(
     repo = "github.com/fgerick/Mire.jl.git",
+    deploy_config = Documenter.GitHubActions()
 )
