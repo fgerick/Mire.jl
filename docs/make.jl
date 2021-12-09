596e367a0e9b33928911fea518b9d402263d95db
Felix <felixgerick@gmail.com>
2021-12-09 14:08:03 +0100

    rm compat

--- docs/make.jl
+++ docs/make.jl
@@ -2,6 +2,5 @@ using Documenter, Mire
 
 makedocs(sitename = "Mire.jl - Modes In Rotating Ellipsoids Julia package")
 deploydocs(
-    repo = "github.com/fgerick/Mire.jl.git",
-    push_preview=false
+    repo = "github.com/fgerick/Mire.jl.git"
 )
